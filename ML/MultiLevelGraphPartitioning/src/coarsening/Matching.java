a886b2627fbc64321a9c0b62fd3709290f98ace3
omar <omar@omar-Latitude-E5410>
2016-10-07 14:12:09 +0200

    test.

--- ML/MultiLevelGraphPartitioning/src/coarsening/Matching.java
+++ ML/MultiLevelGraphPartitioning/src/coarsening/Matching.java
@@ -8,6 +8,7 @@ public abstract class Matching {
 	public abstract ArrayList<ArrayList<Integer>> coarse(Graph graph);
 	
 	public String getSchemeName(){
+		
 		String name = this.getClass().getName();
 		return name;
 	}
